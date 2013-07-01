4e03b6691193ac4fd363e397906c79b2cce57249
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-06-30 23:41:52 -0700

    clean-up onMessage

--- src/background.js
+++ src/background.js
@@ -7,7 +7,6 @@ chrome.browserAction.onClicked.addListener(function() {
   });
 }); 
 
-
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'redirectImages') {
     console.log(request.images);
