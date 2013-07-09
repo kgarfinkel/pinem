d122ed79e7bede7aac1e352b24e7e8b2aaa0aec9
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-07-08 19:42:45 -0700

    send post request for each selected image

--- src/boardScript.js
+++ src/boardScript.js
@@ -5,7 +5,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(images);
     setUp();
     setUpOverlay(images);
-    setUppEvents(images);
+    setUpEvents(images);
   }
 });
 
@@ -48,28 +48,30 @@ function setUpOverlay(images) {
 
 function postCreate(boardId, imgURL, link, description) {
   //var description = input || '';
-  var data = '{"options":{"board_id":"' + boardId + '","description":"","link":"' + link + '","image_url":"' + imgURL + ',"method":"scraped"},"context":{"app_version":"b80ee78"}}';
+  var data = '{"options":{"board_id":"' + boardId + '","description":"","link":"' + link + '","image_url":"' + imgURL + '","method":"scraped"},"context":{"app_version":"5ad1cd"}}';
+
 
   $.post('//pinterest.com/resource/PinResource/create/',
     {data: data,
     source_url : '/pin/find/?url=' + encodeURIComponent(link),
     module_path : 'App()>ImagesFeedPage(resource=FindPinImagesResource(url=' + link + '))>Grid()>GridItems()>Pinnable(url='+ imgURL + ', type=pinnable, link=' + link + ')#Modal(module=PinCreate())'
     }, 
-    function() {
-    console.log('completed');
-  }, 'jsonp');
+    function(response) {
+    console.log('success!');
+  });
 };
 
 function setUpEvents(images) {
-  $('#test').click(function(e) {
-    console.log('post')
+  $('#test').on('click',function(e) {
     e.preventDefault();
+    console.log('clicked')
 
     $(images).each(function(image) {
       var boardId = "238831655177979124",
           imgURL = this.src,
           link = this.href;
-        postCreate(boardId,imgURL,link); 
+          console.log('clicked 2')
+        postCreate(boardId, imgURL, link ); 
     });  
   });
-};
+}
